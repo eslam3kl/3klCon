12cc3797f161bfebb4bcbe219c183f63b48c5eba
Eslam Akl <65075282+eslam3kl@users.noreply.github.com>
2020-11-15 17:45:36 -0800

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -17,10 +17,12 @@ select choice in "${choices[@]}"; do
 					echo "Installing Golang"
 					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
 					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
-					export GOROOT=~/go
+					cp -r go /usr/local/
+					export GOROOT=/usr/local/go
 					export GOPATH=~/go
 					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-					echo 'export GOROOT=/root/go' >> ~/.bash_profile;echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile;
+					echo 'export GOPATH=/root/go'	>> ~/.bash_profile			
 					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
 					source ~/.bash_profile
 					sleep 1
@@ -36,6 +38,7 @@ done
 fi
 
 #install script languages 
+sudo apt install golang-go
 sudo apt-get install -y python3;
 sudo apt-get install -y python3-pip;
 sudo apt-get install -y python-pip; 
@@ -96,6 +99,9 @@ mkdir tools/3klector;
 mv crtfinder.py tools/crtfinder; 
 mv 3klector.py tools/3klector; 
 
+#change priv of bash scripts 
+chmod +x scan.sh; 
+chmod +x gitdorks.sh; 
 
 cd tools/; 
 # current directory --> ~/3klcon/tools
@@ -116,6 +122,7 @@ cp httpx /usr/local/bin/;
 cd ../../../ ;
 #3klcon/tools/
 
+###
 #install nuclei 
 git clone https://github.com/projectdiscovery/nuclei.git; 
 cd nuclei/v2/cmd/nuclei/; 
